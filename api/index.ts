8574e6e77226eefaabe6dd9d252537b87e81f508
Yashvardhan Sharma <yashvardhansharma111@gmail.com>
2025-11-04 17:45:54 +0530

    vercel

--- api/index.ts
+++ api/index.ts
@@ -6,5 +6,11 @@
 import app from "../src/app";
 
 // Export the Express app as a serverless function
+// Vercel will automatically handle the routing
 export default app;
+
+// Configure function timeout (max 30 seconds for Pro plan)
+export const config = {
+  maxDuration: 30,
+};
 
